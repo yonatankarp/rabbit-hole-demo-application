2846736f99d5a78440a28b2446c82b8b43af42e6
yonatankarp <yonatan.karp@sumup.com>
2021-03-26 06:21:04 +0100

    Init project

---
+++ src/main/java/com/yonatankarp/rabbitholedemoapplication/RabbitHoleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yonatankarp.rabbitholedemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHoleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitHoleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yonatankarp/rabbitholedemoapplication/RabbitHoleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonatankarp.rabbitholedemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitHoleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
